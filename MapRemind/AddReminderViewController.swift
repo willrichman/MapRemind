e29dc99cca419edb129045aeac6bb074bf292816
Will Richman <wrichman@gmail.com>
2014-11-03 13:21:47 -0800

    Created an AddReminder ViewController and custom class.

---
+++ MapRemind/AddReminderViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AddReminderViewController.swift
+//  MapRemind
+//
+//  Created by William Richman on 11/3/14.
+//  Copyright (c) 2014 Will Richman. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import MapKit
+
+class AddReminderViewController: UIViewController {
+    
+    var locationManager : CLLocationManager!
+    var selectedAnnotation : MKAnnotation!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
